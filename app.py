c9445af69f1068c22fb3fc050fb0a36603b9e209
aryama <aryamasinha864@gmail.com>
2021-06-01 22:18:34 +0530

    routing changes

--- app.py
+++ app.py
@@ -6,5 +6,19 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/startup')
+def startup():
+    return render_template('startup_form.html')
+
+
+@app.route('/datascience')
+def datascience():
+    return render_template('data_science_form.html')
+
+
+@app.route('/result')
+def result():
+    return render_template('score.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
